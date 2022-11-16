1af51e8e617661a620411e322d4664c62c87c96c
taftadjani <taftadjani@gmail.com>
2022-11-16 23:36:25 +0100

    login an account

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,29 +2,38 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+	use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	protected $fillable = [
+		'name',
+		'first_name',
+		'last_name',
+		'email',
+		'password',
+	];
 
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
+
+	protected function active(): Attribute
+	{
+		return Attribute::make(
+			get: fn ($value) => $value == 1,
+		);
+	}
 }
